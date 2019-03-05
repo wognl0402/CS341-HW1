d6a8eaf296b96c462414e20e6694e829f52fadbd
wognl0402 <wognl0402@gmail.com>
2019-03-05 20:47:03 +0900

    client with basic functionality

--- client.c
+++ client.c
@@ -12,9 +12,9 @@
 #define MAX_BUF 1000000
 
 //char BUF[BUFSIZ];
-
+char *inputString(FILE*, size_t);
 unsigned short checksum(const char *, unsigned);
-void build_header(char *, int);
+void build_header(char *, int, unsigned char, unsigned char);
 
 int main(int argc, char **argv){
 	// Assume command line format is set as follow
@@ -24,16 +24,21 @@ int main(int argc, char **argv){
 	int len;
 	int n;
 
-	char buf[MAX_BUF];
+	unsigned char op;
+	unsigned char shift;
+	//char buf[MAX_BUF];
 	char rec_buf[MAX_BUF];
 
-
+	char *m;
 
 	memset(&c_addr, 0, sizeof(c_addr));
 	c_addr.sin_addr.s_addr = inet_addr(argv[2]);
 	c_addr.sin_family = AF_INET;
 	c_addr.sin_port = htons(atoi(argv[4]));
 	//c_addr.sin_port = htons(5000);
+
+	op = atoi(argv[6]);
+	shift = atoi(argv[8]);
 
 	printf("Let's");
 	if((c_socket = socket(PF_INET, SOCK_STREAM, 0))<0){
@@ -47,23 +52,25 @@ int main(int argc, char **argv){
 		return -1;
 	}
 	printf(" connection!\n");
-	memset(buf, 0 , BUFSIZ);
+	//memset(buf, 0 , BUFSIZ);
 
 	printf("20140496> ");
 
-	fgets(buf,256,stdin);
+	char *buf;
+	buf = inputString(stdin, 12);
+	//fgets(buf,256,stdin);
 
 
-	//char * msg;
 	char * msg = (char *) calloc(strlen(buf)+8, sizeof(char));
 
 	printf("strlen of buf %d\n", strlen(buf));
 	memcpy(msg+8, buf, strlen(buf));
-	build_header(msg,strlen(buf));
+	build_header(msg,strlen(buf), op, shift);
 	printf("strlen of buf %d\n", strlen(msg));
 	printf(msg+8);
 	
-	//send(c_socket,msg,strlen(buf)+8, 0);
+	send(c_socket,msg,strlen(buf)+8, 0);
+	/*
 	int checks;
 	printf("send it two times....\n");
 	checks = send(c_socket,msg, 10, 0);
@@ -71,15 +78,21 @@ int main(int argc, char **argv){
 	sleep(1);
 	checks = send(c_socket,msg+10 , strlen(buf)-2, 0);
 	printf("Second shot.. %d\n", checks);
-	
-	unsigned int rec = recv(c_socket, buff, strlen(buf)+8, 0);
+	*/
+	unsigned int rec = recv(c_socket, rec_buf, strlen(buf)+8, 0);
 	printf("rec[%d] vs strlen[%d]\n", rec, strlen(buf)+8);
 	printf("rec is %d\n", rec);
-	printf(buff+8);
+	printf(rec_buf+8);
 
+	free(m);
 	close(c_socket);
 }
+void send_chunk(const char *buf){
+	char *msg = (char *) calloc(strlen(buf)+8, sizeof(char));
 
+	memcpy (msg+8, buf, strlen(buf));
+	//build_header(msg, strlen(buf));
+}
 unsigned short checksum(const char *buf, unsigned size){
 	unsigned sum = 0;
 	int i;
@@ -101,7 +114,7 @@ unsigned short checksum(const char *buf, unsigned size){
 	return ~sum;
 }
 //void build_header (char* h, unsigned char op, unsigned char shift, unsigned int length){
-void build_header (char* h, int len){
+void build_header (char* h, int len, unsigned char op, unsigned char shift){
 	/*
 	*(unsigned char*)(h) = 'x';
 	*(unsigned char*)(h+1) = 'x';
@@ -109,8 +122,27 @@ void build_header (char* h, int len){
 	*(unsigned short*)(h+2) = checksum(h, length);
 	*/
 	int length = len +8;
-	*(unsigned char*)(h) = 0x01;
-	*(unsigned char*)(h+1) = 0x04;
+	*(unsigned char*)(h) = op;
+	*(unsigned char*)(h+1) = shift;
 	*(int*)(h+4) = htonl(length);
 	*(unsigned short*)(h+2) = checksum(h, length);
+}
+
+char *inputString(FILE* fp, size_t size){
+//The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char)*size);//size is start size
+    if(!str)return str;
+    while(EOF!=(ch=fgetc(fp))/* && ch != '\n'*/ ){
+        str[len++]=ch;
+        if(len==size){
+            str = realloc(str, sizeof(char)*(size+=10));
+            if(!str)return str;
+        }
+    }
+    str[len++]='\0';
+
+    return realloc(str, sizeof(char)*len);
 }
