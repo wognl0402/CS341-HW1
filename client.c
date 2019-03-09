c310532fd3e2aa564a3fd755e76b5c6eea87fcf0
wognl0402 <wognl0402@gmail.com>
2019-03-09 20:55:15 +0900

    finalize

--- client.c
+++ client.c
@@ -18,22 +18,13 @@ unsigned short checksum(const char *, unsigned);
 unsigned short checksum2(const char *, unsigned);
 void build_header(char *, unsigned int, unsigned char, unsigned char);
 void op_chunk(int, char *, size_t, unsigned char, unsigned char);
-void receiver(int, char *, size_t);
 size_t part_receiver(int , char *);
 
 int main(int argc, char **argv){
-	// Assume command line format is set as follow
-	// $ ./client -h [IP] -p [PORT] -o [op] -s [shift]
 	int c_socket;
 	struct sockaddr_in c_addr;
 	int len;
 	int n;
-
-
-	//char buf[MAX_BUF];
-
-
-	char *m;
 
 	char h[BUFSIZE];
 	char p[BUFSIZE];
@@ -61,128 +52,53 @@ int main(int argc, char **argv){
 				exit(0);
 		}
 	}
+
 	memset(&c_addr, 0, sizeof(c_addr));
 	c_addr.sin_addr.s_addr = inet_addr(h);
 	c_addr.sin_family = AF_INET;
 	c_addr.sin_port = htons(atoi(p));
-	//c_addr.sin_port = htons(5000);
 
 	op = atoi(o);
 	shift = atoi(s);
 
 	if((c_socket = socket(PF_INET, SOCK_STREAM, 0))<0){
-		printf("Can't create\n");
 		return -1;
 	}
 	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1) {
-		printf("CAN'T CONNECT\n");
 		close(c_socket);
 		return -1;
 	}
-	//memset(buf, 0 , BUFSIZ);
-
 
 	char *buf;
 	buf = inputString(stdin, 12);
-	//fgets(buf,256,stdin);
 
 	size_t length = strlen(buf);
 	while(length >= MAX_BUF - 8){
-		//char *chunk = (char *) calloc(MAX_BUF, sizeof(char));
 		op_chunk (c_socket, buf, MAX_BUF - 8, op, shift);
-
 		buf = buf + MAX_BUF -8;
 		length = length - (MAX_BUF - 8);
-		//memcpy (chunk
 	}
-
 	op_chunk(c_socket, buf, length, op, shift);
-	//printf(buf);
 
-	exit(0);
-
-	char * msg = (char *) calloc(strlen(buf)+8, sizeof(char));
-
-	memcpy(msg+8, buf, strlen(buf));
-	build_header(msg,strlen(buf), op, shift);
-	printf("strlen of buf %d\n", strlen(msg));
-	printf(msg+8);
-	
-	send(c_socket,msg,strlen(buf)+8, 0);
-	/*
-	int checks;
-	printf("send it two times....\n");
-	checks = send(c_socket,msg, 10, 0);
-	printf("First shot.. %d\n", checks);
-	sleep(1);
-	checks = send(c_socket,msg+10 , strlen(buf)-2, 0);
-	printf("Second shot.. %d\n", checks);
-	*/
-
-	/*
-	unsigned int rec = recv(c_socket, rec_buf, strlen(buf)+8, 0);
-	printf("rec[%d] vs strlen[%d]\n", rec, strlen(buf)+8);
-	printf("rec is %d\n", rec);
-	printf(rec_buf+8);
-
-	free(m);
-	close(c_socket);
-	*/
+	return 0;
 }
 void op_chunk(int c_socket, char *start, size_t length, unsigned char op, unsigned char shift){
-	char *msg = (char *) calloc (length+9, sizeof(char));
+	char *msg = (char *) calloc (length+8, sizeof(char));
 	memcpy (msg+8, start, length);
 	build_header(msg, length+8, op, shift);
 
 	send(c_socket, msg, length+8, 0);
 
 	free(msg);
-	char *rcv_msg = (char *) calloc (length, sizeof(char));
+	char *rcv_msg = (char *) calloc (length+8, sizeof(char));
 	
-	receiver(c_socket, rcv_msg, length);
-	/*
-	int rec = recv(c_socket, rcv_msg, length+8, 0);
-	*/
-	/*
-	if (checksum(rcv_msg, length+8) != 0){
-		printf("[length+8:%u] [rec:%d]\n", length+8, strlen(rcv_msg+8));
-		printf("[chcksm: %x]\n",checksum(rcv_msg, length+8));
-		printf("Something went wrong\n");
-		for (int j = 0; j<4 ; j++){
-			printf("%x\n", rcv_msg[j]);
-		}
-		printf("rec length [%u][%d]\n", rcv_msg+4, rcv_msg+4);
-		exit(0);
-	}
-	*/
+	size_t rcv_length = part_receiver(c_socket, rcv_msg);
 	for (int i = 0; i<length ; i++){
-		//printf("seg?\n");
-		printf("%c", rcv_msg[i]);
-
+		printf("%c", rcv_msg[i+8]);
 	}
-	//printf(msg+8);
 	free(rcv_msg);
 }
-void receiver(int c_socket, char *receiver, size_t length){
-	//initially wait for at least 8 bytes
-	size_t rec_total = 0;
-	while(rec_total != length){
-		
-		//char p_receiver[MAX_BUF];
-		char * p_receiver = calloc(MAX_BUF, sizeof(char));
-		size_t p_length = part_receiver(c_socket, p_receiver);
-		
-		//printf("Now @ receiver, p_length[%u]\n", p_length);
-		memcpy(receiver+rec_total, p_receiver+8, p_length -8);
-		rec_total += p_length - 8;
-		free(p_receiver);
-		//int rec = recv(c_socket, receiver+rec_total, length-rec_total, 0);
-		//rec_total += rec;
-	}
-	//printf("rec_total[%u] length[%u]\n", rec_total, length);
-}
 size_t part_receiver(int c_socket, char *receiver){
-	//first see 8 bytes
 	size_t rec_part = 0;
 	while(rec_part < 8){
 		int rec = recv(c_socket, receiver+rec_part, 8-rec_part, 0);
@@ -193,19 +109,13 @@ size_t part_receiver(int c_socket, char *receiver){
 		exit(0);
 	}
 	size_t new_length = ntohl(*(unsigned int *)(receiver+4));
-	//printf("at part_receiver, rec_part[%u]\n", rec_part);
 
 	while(rec_part != new_length){
 		int rec = recv(c_socket, receiver+rec_part, new_length-rec_part, 0);
 		rec_part += rec;
-		//printf("[%d]\n", rec_part);
 	}
 	if (checksum2(receiver, new_length) != 0){
-		//printf("[length+8:%u] [rec:%d]\n", length+8, strlen(rcv_msg+8));
-	
-		printf("what [%u], [%u]\n", rec_part, new_length);
-		printf("[chcksm: %x]\n",checksum2(receiver, new_length));
-		printf("Something went wrong\n");
+		close(c_socket);
 		exit(0);
 	}
 	return new_length;
@@ -230,14 +140,7 @@ unsigned short checksum(const char *buf, unsigned size){
 
 	return ~sum;
 }
-//void build_header (char* h, unsigned char op, unsigned char shift, unsigned int length){
 void build_header (char* h, unsigned int length, unsigned char op, unsigned char shift){
-	/*
-	*(unsigned char*)(h) = 'x';
-	*(unsigned char*)(h+1) = 'x';
-	*(unsigned int*)(h+4) = (unsigned int) 'zzzz';
-	*(unsigned short*)(h+2) = checksum(h, length);
-	*/
 	if (op!=1 && op!= 0){
 		exit(0);
 	}
@@ -248,11 +151,10 @@ void build_header (char* h, unsigned int length, unsigned char op, unsigned char
 }
 
 char *inputString(FILE* fp, size_t size){
-//The size is extended by the input with the value of the provisional
     char *str;
     int ch;
     size_t len = 0;
-    str = realloc(NULL, sizeof(char)*size);//size is start size
+    str = realloc(NULL, sizeof(char)*size);
     if(!str)return str;
     while(EOF!=(ch=fgetc(fp))/* && ch != '\n'*/ ){
         str[len++]=ch;
@@ -261,7 +163,7 @@ char *inputString(FILE* fp, size_t size){
             if(!str)return str;
         }
     }
-    str[len++]='\0';
+    //str[len++]='\0';
 
     return realloc(str, sizeof(char)*len);
 }
--- server.c
+++ server.c
@@ -12,7 +12,7 @@
 #define MAX_BUF 1000000 
 #define BUFSIZE 25
 void receiver(int, char *, size_t);
-size_t part_receiver(int , char *);
+size_t part_receiver(int , char **);
 
 //void encrypt (char *, unsigned, unsigned char, unsigned char);
 void encrypt (char *);
@@ -41,11 +41,8 @@ int main(int argc, char *argv[]){
 				exit(0);
 		}
 	}
-
-	printf("=Server Start\n");
 	server_socket = socket(PF_INET, SOCK_STREAM, 0);
 	if(server_socket<0){
-		printf("%s \n", "fail to make socket");
 		exit(1);
 	}
 
@@ -56,12 +53,10 @@ int main(int argc, char *argv[]){
 	/**Designate port and ip address**/
 
 	if(bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr))<0){
-		printf("%s \n", "bind error");
 		exit(1);
 	}
 
-	if(listen(server_socket,100)<0){
-		printf("%s \n","listen error");
+	if(listen(server_socket,200)<0){
 		exit(1);
 	}
 	/**Coneect and set  socket**/
@@ -72,7 +67,6 @@ int main(int argc, char *argv[]){
 		client_addr_size = sizeof (client_addr);
 		client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_addr_size);
 		if (client_socket <0){
-			printf("can't cbind\n");
 			exit(1);
 		}
 		int pid = fork();
@@ -82,17 +76,19 @@ int main(int argc, char *argv[]){
 		if (pid == 0){
 			//temporary
 			while(1){
-			char * rcv_msg = (char *) calloc (MAX_BUF, sizeof (char));
+			//char * rcv_msg = (char *) calloc (MAX_BUF, sizeof (char));
 			int rec;
+			char *rcv_msg;
 			
 			
 			//rec = recv(client_socket, rcv_msg, 25, 0);
-			size_t rcv_length = part_receiver(client_socket, rcv_msg);
+			size_t rcv_length = part_receiver(client_socket, &rcv_msg);
 
-			if (rcv_length == 0)
+			if (rcv_length == 0){
+				free(rcv_msg);
 				break;
+			}
 			if (rec < 0){
-				printf("rec error\n");
 				exit(1);
 			}
 			encrypt(rcv_msg);
@@ -100,95 +96,47 @@ int main(int argc, char *argv[]){
 			*(unsigned short*) (rcv_msg+2) = checksum2(rcv_msg, rcv_length);
 			send(client_socket, rcv_msg, rcv_length,0);
 
-			/*
-			rec = recv(client_socket, buffer, BUFSIZ, 0);
-			if (rec < 0){
-				printf("rec error\n");
-				exit(1);
-			}
-			printf("Received Message is like \n");
-			printf(buffer);
-			*/
+			free(rcv_msg);
 			}
 			close(client_socket);
 			exit(1);
-
-
 		}
 		if (pid > 0){
 			close(client_socket);
 		}
 	}
 	exit(0);
-	while(1){
-		client_addr_size = sizeof(client_addr);
-		client_socket = accept(server_socket,(struct sockaddr*)&client_addr,&client_addr_size);/**Wait until client socket is connected**/
-		printf("=================OK Server\n");
-
-		if(client_socket<0){
-			printf("%s \n","cleint connection error");
-			exit(1);
-		}
-
-		//read(client_socket, buff,BUFSIZ);/**Receive the signal**/
-		printf("client saids... \n");
-		printf(buff+8);
-		close(client_socket);
-
-	}
 }
-void receiver(int c_socket, char *receiver, size_t length){
-	//initially wait for at least 8 bytes
-	size_t rec_total = 0;
-	while(rec_total != length){
-		
-		//char p_receiver[MAX_BUF];
-		char * p_receiver = calloc(MAX_BUF, sizeof(char));
-		size_t p_length = part_receiver(c_socket, p_receiver);
-		
-		//printf("Now @ receiver, p_length[%u]\n", p_length);
-		memcpy(receiver+rec_total, p_receiver+8, p_length -8);
-		rec_total += p_length - 8;
-		free(p_receiver);
-		//int rec = recv(c_socket, receiver+rec_total, length-rec_total, 0);
-		//rec_total += rec;
-	}
-	//printf("rec_total[%u] length[%u]\n", rec_total, length);
-}
-size_t part_receiver(int c_socket, char *receiver){
-	//first see 8 bytes
+size_t part_receiver(int c_socket, char **receiver){
+	char *temp;
+	temp = realloc(NULL, sizeof(char)*8);
+
 	size_t rec_part = 0;
 	while(rec_part < 8){
-		int rec = recv(c_socket, receiver+rec_part, 8-rec_part, 0);
+		int rec = recv(c_socket, temp+rec_part, 8-rec_part, 0);
 		rec_part += rec;
 		if (rec == 0)
 			return 0;
-		//printf("this core? %d\n", rec_part);
 	}
-	unsigned char op = *(unsigned char *)receiver;
+	unsigned char op = *(unsigned char *)temp;
 	if ((op != 0) && (op!= 1)){
+		close(c_socket);
 		exit(0);
 	}
-	size_t new_length = ntohl(*(unsigned int *)(receiver+4));
-	//printf("at part_receiver, rec_part[%u]\n", rec_part);
+	size_t new_length = ntohl(*(unsigned int *)(temp+4));
+	temp = realloc(temp, sizeof(char)*new_length);
 
 	while(rec_part != new_length){
-		//printf("this core??\n");
-		int rec = recv(c_socket, receiver+rec_part, new_length-rec_part, 0);
+		int rec = recv(c_socket, temp+rec_part, new_length-rec_part, 0);
 		rec_part += rec;
-		//printf("[%d]\n", rec_part);
 	}
-	if (checksum2(receiver, new_length) != 0){
-		//printf("[length+8:%u] [rec:%d]\n", length+8, strlen(rcv_msg+8));
-	
-		printf("what [%u], [%u]\n", rec_part, new_length);
-		printf("[chcksm: %x]\n",checksum2(receiver, new_length));
-		printf("Something went wrong\n");
+	if (checksum2(temp, new_length) != 0){
+		close(c_socket);
 		exit(0);
 	}
+	*receiver = temp;
 	return new_length;
 }
-
 void encrypt(char *msg){
 	unsigned char op = *(unsigned char *) msg;
 	unsigned char shift = *(unsigned char *) (msg+1);
@@ -217,7 +165,6 @@ void cipher(char *letter, unsigned char op, unsigned char shift){
 	}
 
 	temp -= 97;
-
 	temp += shifter;
 
 	temp = temp % 26;
